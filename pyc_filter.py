0b50afdd407763b3460263ac3b8b97983e5045ba
ceschi <emanuele.franceschi@gmail.com>
2019-08-09 05:35:26 +0200

    removed script name from file list, stuff for prog bar is there

--- pyc_filter.py
+++ pyc_filter.py
@@ -1,19 +1,25 @@
-# python script to select images
 
+'''
+Very simple script to split videos
+in their frames and then do things
+on single frames
+'''
 
 
 # import a bunch of pkgs and fcts
 
-import skimage
-from skimage import data, io
+# import skimage
+# from skimage import data, io
 import numpy as np
 from matplotlib import pyplot as plt
 import shutil
+from progressbar import ProgressBar
 
-
-import os, sys
+import os
+from os.path import join
 import time as timer
 
+pbar = ProgressBar()
 start_t = timer.time()
 
 ###################################
@@ -22,48 +28,52 @@ tol = 1.8 # tolerance level, critical!
 
 
 # cool, go to test dir
-os.chdir(r"C:\Users\emanu\jpy_ntbs\pics")
+# os.chdir(r"C:\Users\emanu\jpy_ntbs\pics"))
+
+# now, the script shall operate within
+# the folder where the frames are,
+# so it mainly works on local wd
+
+wd = os.getcwd()
 
 # get a list of all files excluding folders
 # make sure it's only jpg later
 
-photo_files = [i for i in os.listdir(os.getcwd()) if os.path.isfile( os.path.join( os.getcwd(), i ) )]
+photo_files = [i for i in os.listdir(wd) if os.path.isfile(join( wd, i ) )]
+photo_files.remove('pyc_filter.py')
+
 
 # now create apposite dir if
 # not existent already
-if os.path.isdir('./filter_dir'):
+if os.path.isdir('./selected_frames'):
     print('Folder already present')
 else:
-    os.mkdir('filter_dir')
+    os.mkdir('selected_frames')
     print('Folder created')
 
 # this will expand to get all photos' names
 # that are good
 good_ones =[]
-dest_path = r"C:\Users\emanu\jpy_ntbs\pics\filter_dir"
+dest_path = join(wd, 'selected_frames')
 
 # now, the big big loop
 
 start_loop = timer.time()
 
-for i in photo_files:    
+for i in pbar(photo_files):    
     img = plt.imread(i)
     thresh = np.mean(img, axis = 2).mean()
     
     if thresh>=tol:
         good_ones.append(i)
-        cp_path = os.path.join(os.getcwd(), i)
-        shutil.copy(cp_path, os.path.join(dest_path, i))
+        cp_path = join(wd, i)
+        shutil.copy(cp_path, join(dest_path, i))
 
 end_loop = timer.time()
 
 
-start_moving = timer.time()
-#for j in good_ones:
- #   cp_path = os.path.join(os.getcwd(), j)
-  #  shutil.copy(cp_path, os.path.join(dest_path, j))
-    
-end_moving = timer.time()
 
-print('loop with %s files done in %f minutes: %d files were kept!\n\nMoving took %f seconds' %
-      (len(photo_files), (end_loop - start_loop)/60, len(good_ones), (end_moving - start_moving)))
+
+print('loop with %s files done in %f minutes: %d files were kept!' %
+      (len(photo_files), (end_loop - start_loop)/60, len(good_ones))
+      )
